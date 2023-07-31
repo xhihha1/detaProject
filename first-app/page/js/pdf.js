4cc5c5ac31029b695ac1f4e14702d6442fbc7bb1
ken.yek <ken.yek@advantech.com.tw>
2023-07-31 17:15:55 +0800

    update

--- first-app/page/js/pdf.js
+++ first-app/page/js/pdf.js
@@ -6,3 +6,28 @@
 // When the value is a Blob
 // formData.append("userpic", myFileInput.files[0], "chris1.jpg");
 // formData.append("userpic", myFileInput.files[1], "chris2.jpg");
+
+// fetch(img.src)
+//     .then(res => res.blob())
+//     .then(blob => {
+//         const file = new File([blob], "capture.png", {
+//             type: 'image/png'
+//         });
+//         var fd = new FormData();
+//         fd.append("image", file);
+//         $.ajax({
+//             type: "POST",
+//             enctype: 'multipart/form-data',
+//             url: "/api/file/upload",
+//             data: fd,
+//             processData: false,
+//             contentType: false,
+//             cache: false,
+//             success: (data) => {
+//                 alert("yes");
+//             },
+//             error: function(xhr, status, error) {
+//                 alert(xhr.responseText);
+//             }
+//         });
+//     });
