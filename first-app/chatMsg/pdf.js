a3d69f518dabc6814bc6d3ebf7e7cc4e8c0a296d
ken.yek <ken.yek@advantech.com.tw>
2023-07-31 17:10:55 +0800

    update

--- first-app/chatMsg/pdf.js
+++ first-app/chatMsg/pdf.js
@@ -2,7 +2,8 @@ var express = require('express');
 const multer = require('multer');
 const mime = require("mime")
 var router = express.Router();
-const PDFDocument = require('pdfkit');
+const PDFKitDocument = require('pdfkit');
+const pdfLib = require('pdf-lib');
 const fs = require('fs');
 const path = require('path');
 const {
@@ -25,7 +26,7 @@ router.post('/convert', cpUpload, (req, res) => {
   if (req.files && req.files['images']) {
     const files = req.files['images'];
     // Custom sort function to sort files numerically based on original names
-    const pdf = new PDFDocument();
+    const pdf = new PDFKitDocument();
     files.sort((a, b) => {
       const fileAIndex = parseInt(a.originalname);
       const fileBIndex = parseInt(b.originalname);
@@ -51,6 +52,42 @@ router.post('/convert', cpUpload, (req, res) => {
     });
   }
 
+});
+
+
+const cpUploadpdfs = upload.fields([{
+  name: 'pdfs',
+  maxCount: 30
+}])
+// Handle the PDF merge route
+router.post('/merge', cpUploadpdfs, async (req, res) => {
+  try {
+    // const { files } = req;
+    const mergedPdf = await pdfLib.PDFDocument.create();
+    let files;
+    if (req.files && req.files['pdfs']) {
+      files = req.files['pdfs'];
+    }
+
+    for (const file of files) {
+      const pdfBytes = file.buffer;
+      const pdfDoc = await pdfLib.PDFDocument.load(pdfBytes);
+      const pdfPages = await mergedPdf.copyPages(pdfDoc, pdfDoc.getPageIndices());
+      pdfPages.forEach((page) => mergedPdf.addPage(page));
+    }
+
+    const mergedPdfBytes = await mergedPdf.save();
+    const mergedPdfPath = 'merged.pdf';
+
+    await fs.promises.writeFile(mergedPdfPath, mergedPdfBytes);
+
+    res.download(mergedPdfPath, 'merged.pdf', () => {
+      fs.unlinkSync(mergedPdfPath);
+    });
+  } catch (err) {
+    console.error('Error merging PDFs:', err);
+    res.status(500).send('Error merging PDFs');
+  }
 });
 
 
---
+++ first-app/page/js/pdf.js
@@ -0,0 +1,8 @@
+// const galleryData = new FormData()
+// for (let i = 0; i < files.length; i++) {
+//    galleryData.append('images[]', file[i])
+// }
+
+// When the value is a Blob
+// formData.append("userpic", myFileInput.files[0], "chris1.jpg");
+// formData.append("userpic", myFileInput.files[1], "chris2.jpg");
